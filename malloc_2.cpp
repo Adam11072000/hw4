824eed5b1a685f7503210ee00e41efb5c0dd99e1
Adam11072000 <ghadbanadam@gmail.com>
2022-01-14 14:02:44 +0000

    part 2 done baby

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,281 +1,157 @@
 //
 // Created by student on 1/13/22.
 //
-#include <cassert>
 #include <unistd.h>
+#include <cstring>
+#include <cmath>
 
-typedef enum ListResult_t{
-    LIST_FAIL,
-    LIST_SUCCESS
-} ListResult;
+const long int MAX_SIZE = pow(10, 8);
 
-
-class MetaData{
-    int size;
+struct MetaData{
+    size_t size;
     bool is_free;
     MetaData* prev;
     MetaData* next;
-    void* ptr; // start of block to user
-public:
-    MetaData(int size, void* ptr, MetaData* prev = nullptr, MetaData* next = nullptr): size(size), ptr(ptr), prev(prev), next(next) {}
-    MetaData(const MetaData& src): size(src.size), is_free(src.is_free), prev(src.prev), next(src.next){}
-    ~MetaData() = default; // we may need to change
-    int getSize() const;
-    void setSize(int size);
-    void setNext(MetaData* next);
-    MetaData* getNext() const;
-    MetaData* getPrevious() const;
-    void setPrevious(MetaData* prev);
-    void setPtr(void* ptr);
-    void* getPtr() const;
 };
 
-MetaData* MetaData::getNext() const{
-    return next;
-}
+MetaData* heapHead = NULL;
+MetaData* heapTail = NULL; // last element
+int count = 0;
+int free_blocks = 0;
 
-void MetaData::setNext(MetaData* nextNode){
-    this->next = nextNode;
-}
 
-MetaData* MetaData::getPrevious() const{
-    return prev;
-}
 
-void MetaData::setPrevious(MetaData* previous){
-    this->prev = previous;
-}
-
-void* MetaData::getPtr() const{
-    return ptr;
-}
-
-void MetaData::setPtr(void* ptr){
-    this->ptr = ptr;
-}
-
-int MetaData::getSize() const{
-    return size;
-}
-
-void MetaData::setSize(int size) {
-    this->size = size;
-}
-
-class LinkedList{
-public:
-    MetaData* head;
-    MetaData* tail;
-    int size;
-
-public:
-    LinkedList():head(nullptr), tail(nullptr),size(0){}
-    LinkedList(const LinkedList& src);
-    ~LinkedList();
-    LinkedList& operator=(const LinkedList& src);
-    MetaData* find(const MetaData& data);
-    ListResult insert(const MetaData& data);
-    ListResult insertWithSort(const MetaData& data);
-    int getSize();
-    ListResult remove(const MetaData& data);
-    void printList();
-    MetaData* getHead() const;
-    void deleteList();
-    ListResult deleteByPointer(MetaData* toDelete);
-};
-
-ListResult LinkedList::deleteByPointer(MetaData* toDelete){
-    assert(toDelete);
-    if(head == nullptr && tail == nullptr){
-        return LIST_FAIL;
+void* smalloc(size_t size){
+    if(size > MAX_SIZE || size == 0){
+        return NULL;
     }
-    if(toDelete == head){
-        head = toDelete->getNext();
-    }
-    if(toDelete == tail){
-        tail = toDelete->getPrevious();
-    }
-    if(toDelete->getPrevious()){
-        toDelete->getPrevious()->setNext(toDelete->getNext());
-    }
-    if(toDelete->getNext()){
-        toDelete->getNext()->setPrevious(toDelete->getPrevious());
-    }
-    delete toDelete;
-    size--;
-    return LIST_SUCCESS;
-}
+    void* out = NULL;
+    MetaData* it = heapHead;
+    bool found_free_space= false;
 
-void LinkedList::deleteList(){
-    MetaData* tmp = head;
-    while(tmp){
-        tmp = head->getNext();
-        delete head;
-        head = tmp;
-    }
-    head = tail = nullptr;
-}
-
-
-LinkedList& LinkedList::operator=(const LinkedList& src){
-    if(this == &src){
-        return *this;
-    }
-    if(src.head == nullptr && src.tail == nullptr){
-        head = tail = nullptr;
-        size = 0;
-        return *this;
-    }
-    this->deleteList();
-    head = new MetaData(src.head->getSize(),src.head->getPtr());
-    MetaData* iterSrc = src.head;
-    MetaData* iterDest = head;
-    MetaData* prev = nullptr;
-    while(iterSrc){
-        if(iterSrc->getNext()){
-            iterDest->setNext(new MetaData(iterSrc->getSize(), iterSrc->getNext()->getPtr()));
+    if(count == 0){
+        heapHead = (MetaData*)(sbrk(intptr_t (sizeof(MetaData) + size)));
+        if(heapHead == (void*)(-1)){
+            exit(1);
         }
-        else{
-            iterDest->setNext(nullptr);
-            tail = iterDest;
-        }
-        iterDest->setPrevious(prev);
-        prev = iterDest;
-        iterDest = iterDest->getNext();
-        iterSrc = iterSrc->getNext();
-    }
-    return *this;
-}
-
-MetaData* LinkedList::getHead() const{
-    return head;
-}
-
-LinkedList::LinkedList(const LinkedList& src){
-    size=src.size;
-    if(src.head != nullptr && src.tail != nullptr) {
-        head = new MetaData(src.head->getSize(),src.head->getPtr());
-        MetaData *iterSrc = src.head;
-        MetaData *iterDest = head;
-        while (iterSrc) {
-            if (iterSrc->getNext()) {
-                iterDest->setNext(new MetaData(iterSrc->getSize(), iterSrc->getNext()->getPtr()));
-                iterDest->getNext()->setPrevious(iterDest);
-            } else {
-                iterDest->setNext(nullptr);
-                tail = iterDest;
+        heapHead->size = size;
+        heapHead->prev = NULL;
+        heapHead->next = NULL;
+        heapHead->is_free = false;
+        heapTail = heapHead;
+        out = (void*) (heapHead+sizeof(MetaData));
+    } else{
+        while(it != heapTail){
+            if(it->size >= size && it->is_free){
+                it->is_free = false;
+                out = it + sizeof(MetaData);
+                found_free_space = true;
+                free_blocks--;
             }
-            iterDest = iterDest->getNext();
-            iterSrc = iterSrc->getNext();
+            it = it->next;
+        }
+        if(!found_free_space){
+            heapTail->next = (MetaData*)(sbrk(intptr_t (sizeof(MetaData) + size)));
+            if(heapTail == (void*)(-1)){
+                exit(1);
+            }
+            heapTail->next->prev = heapTail;
+            heapTail = heapTail->next;
+            heapTail->next = NULL;
+            heapTail->size = size;
+            heapTail->is_free = false;
+            out = (void*)(heapTail + sizeof(MetaData));
         }
     }
-    else{
-        head = nullptr;
-        tail = nullptr;
-    }
+    count++;
+    return out;
 }
 
-
-LinkedList::~LinkedList(){
-    this->deleteList();
+void* scalloc(size_t num, size_t size){
+    if(size == 0 || size*num > MAX_SIZE){
+        return NULL;
+    }
+    void* out = smalloc(size*num);
+    if(out == NULL){
+        return NULL;
+    }
+    std::memset(out,0, size*num); // check ascii
+    return out;
 }
 
-MetaData* LinkedList::find(const MetaData& data){
-    MetaData* iter = head;
-    while(iter){
-        if(iter->getPtr() == data.getPtr()){
-            return iter;
-        }
-        iter = iter->getNext();
+void sfree(void* p){
+    if(p == NULL){
+        return;
     }
-    return NULL;
-}
-
-ListResult LinkedList::insert(const MetaData& new_data){
-    MetaData* addedMetaData = new MetaData(new_data);
-    if(head == nullptr && tail == nullptr){
-        head = tail = addedMetaData;
-    }
-    else{
-        head->setPrevious(addedMetaData);
-        addedMetaData->setNext(head);
-        head = addedMetaData;
-    }
-
-    size++;
-    return LIST_SUCCESS;
-}
-
-ListResult LinkedList::insertWithSort(const MetaData& new_data){
-    MetaData* iter = head;
-    MetaData* addedMetaData = new MetaData(new_data);
-    if(head == nullptr && tail == nullptr){
-        head = tail = addedMetaData;
-        size++;
-        return LIST_SUCCESS;
-    }
-    while(iter && (new_data.getPtr() > iter->getPtr())){
-        iter = iter->getNext();
-    }
-    if(iter){
-        if(iter->getPrevious()){
-            iter->getPrevious()->setNext(addedMetaData);
-        }else{
-            head = addedMetaData;
-        }
-        addedMetaData->setPrevious(iter->getPrevious());
-        addedMetaData->setNext(iter);
-        iter->setPrevious(addedMetaData);
-    }else{
-        tail->setNext(addedMetaData);
-        addedMetaData->setNext(nullptr);
-        addedMetaData->setPrevious(tail);
-        tail = addedMetaData;
-    }
-    size++;
-    return LIST_SUCCESS;
-}
-
-ListResult LinkedList::remove(const MetaData& data){
-    MetaData* iter = head;
-    while(iter){
-        if(data.getPtr() == iter->getPtr()){
-            if(iter == head){
-                head = head->getNext();
-            }
-
-            if(iter == tail){
-                tail = tail->getPrevious();
-            }
-
-            if(iter->getPrevious()){
-                (iter->getPrevious())->setNext(iter->getNext());
-            }
-
-            if(iter->getNext()){
-                (iter->getNext())->setPrevious(iter->getNext());
-            }
-            delete iter;
+    MetaData* it = heapHead;
+    while(it){
+        if((void*)(it + sizeof(MetaData)) == p){
+            it->is_free = true;
+            p = NULL;
+            free_blocks++;
             break;
         }
-        iter = iter->getNext();
     }
-    if(iter == NULL){
-        return LIST_FAIL;
-    }
-    size--;
-    return LIST_SUCCESS;
 }
 
-int LinkedList::getSize(){
-    return size;
-}
-/*
-void LinkedList::printList() {
-    MetaData* tmp = head;
-    while(tmp){
-        std::cout << tmp->getData() <<", ";
-        tmp = tmp->getNext();
+void* srealloc(void* oldp, size_t size){
+    if(size == 0 or size > MAX_SIZE){
+        return NULL;
     }
+    MetaData* it = heapHead;
+    void* out;
+    while(it){
+        if((void*)(it+sizeof(MetaData)) == oldp){
+            if(it->size >= size){
+                return oldp;
+            }
+            it->is_free = true;
+            break;
+        }
+        it = it->next;
+    }
+    out = smalloc(size);
+    if(out == NULL){
+        return NULL;
+    }
+    memcpy(out, oldp, size);
+    return out;
 }
-*/
+
+size_t _num_free_blocks(){
+    return free_blocks;
+}
+
+size_t _num_free_bytes(){
+    MetaData* it = heapHead;
+    size_t res = 0;
+    while(it){
+        if(it->is_free){
+            res += it->size;
+        }
+        it = it->next;
+    }
+    return res;
+}
+
+size_t _num_allocated_blocks(){
+    return count;
+}
+
+size_t _num_allocated_bytes(){
+    MetaData* it = heapHead;
+    size_t res = 0;
+    while(it){
+        res += it->size;
+        it = it->next;
+    }
+    return res;
+}
+
+size_t _num_meta_data_bytes(){
+    return sizeof(MetaData)*count;
+}
+
+size_t _size_meta_data(){
+    return sizeof(MetaData);
+}
